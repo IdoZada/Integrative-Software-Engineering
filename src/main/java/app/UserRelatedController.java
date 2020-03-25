17a036271167e170c6f4f5534818021946e5fb3d
user <user@MUSAFI.lan>
2020-03-25 19:20:32 +0200

    user related controller 1

--- src/main/java/app/UserRelatedController.java
+++ src/main/java/app/UserRelatedController.java
@@ -1,6 +1,7 @@
 package app;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +15,53 @@ public class UserRelatedController {
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	
-	public UserBoundary newUser (@RequestBody NewUserBoundary newUserBoundary) {
-		return new UserBoundary (newUserBoundary.getRole(), newUserBoundary.getUserName(),
-				new UserId(newUserBoundary.getEmail(), "2020b"));
+	public UserBoundary newUser (@RequestBody NewUserDetails newUserDetails) {
+		
+		return new UserBoundary (newUserDetails.getRole(), newUserDetails.getUserName(),
+				new UserId(newUserDetails.getEmail(), "2020b"));
 	}
+	
+	@RequestMapping(path = "/acs/users/login/{userDomain}/{userEmail}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	
+	public UserBoundary retrieveUserDetails (
+			@PathVariable("userDomain") String userDomain
+			,@PathVariable("userEmail") String userEmail ) {
+		UserBoundary userBoundary = new UserBoundary(
+				Role.PLAYER, "Tomer"
+				, new UserId("tomer@gmail.com", "2020t"));
+		
+		if(userDomain.equals(userBoundary.getUserId().getDomain())
+				&& userEmail.equals(userBoundary.getUserId().getEmail())) {
+			
+			return userBoundary;
+			
+		}else {
+			
+			return new UserBoundary();
+			
+		}
+		
+	}
+	
+	
+	@RequestMapping(path = "/acs/users/{userDomain}/{userEmail}",
+			method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	
+	public void updateUserDetails (
+			@RequestBody UserBoundary userBoundary
+			, @PathVariable("userDomain") String userDomain
+			, @PathVariable("userEmail") String userEmail) {
+		
+		 UserBoundary updateUsBoundary = new UserBoundary (
+				userBoundary.getRole()
+				, userBoundary.getUserName(),
+				new UserId(userBoundary.getUserId().getEmail(), userBoundary.getUserId().getDomain()));
+		 
+		 UserId userId = new UserId(userEmail, userDomain);
+		 updateUsBoundary.setUserId(userId);
+	}
+	
 }
