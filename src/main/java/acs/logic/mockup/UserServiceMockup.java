29850bf5f2a41d4b3e334c254cbaecf4a5cd5911
Aviv <Aviv@DESKTOP-I8GUKU4>
2020-04-05 10:13:33 +0300

    added createUser mockup added login mockup added deleteAllUsers mockup

--- src/main/java/acs/logic/mockup/UserServiceMockup.java
+++ src/main/java/acs/logic/mockup/UserServiceMockup.java
@@ -1,5 +1,52 @@
 package acs.logic.mockup;
 
-public class UserServiceMockup {
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import acs.*;
+import acs.boundary.UserBoundary;
+import acs.logic.UserService;
+
+public class UserServiceMockup implements UserService{
+
+	private static long id = 1000;
+	private Map<UserId,UserBoundary> users = new ConcurrentHashMap<>();
+	
+	@Override
+	public UserBoundary createUser(UserBoundary user) {
+		//TODO: check if need to change UserBoundry param to NewUserDetails
+		if(users.containsKey(user.getUserId())){
+			throw new RuntimeException("User Already exist");
+		}
+		users.put(user.getUserId(), user);
+		return user;
+	}
+
+	@Override
+	public UserBoundary login(String userDomain, String userEmail) {
+		UserId userId = new UserId(userDomain, userEmail);
+		UserBoundary result = users.get(userId);
+		if(result == null)
+			throw new RuntimeException("User Does not exist");
+		return result;
+	}
+
+	@Override
+	public UserBoundary updateUser(String userDomain, String userEmail, UserBoundary update) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserBoundary> getAllUsers(String admainDomain, String admainEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAllUsers(String admainDomain, String admainEmail) {
+		users.clear();
+	}
 
 }
