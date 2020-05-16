66faf0e91fc2cfc3854240692b6701bd3222f8fb
Aviv <Aviv@LAPTOP-3A7UJ667>
2020-05-16 19:07:04 +0300

    updated action service and action tests

--- src/test/java/acs/ActionTests.java
+++ src/test/java/acs/ActionTests.java
@@ -2,10 +2,13 @@ package acs;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import javax.annotation.PostConstruct;
 
+import org.junit.AfterClass;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,10 +19,15 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
 import acs.boundary.ActionBoundary;
+import acs.boundary.ElementBoundary;
+import acs.boundary.UserBoundary;
 import acs.boundary.boundaryUtils.Element;
 import acs.boundary.boundaryUtils.ElementId;
 import acs.boundary.boundaryUtils.InvokedBy;
+import acs.boundary.boundaryUtils.Location;
+import acs.boundary.boundaryUtils.NewUserDetails;
 import acs.boundary.boundaryUtils.UserId;
+import acs.data.UserRole;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -28,6 +36,7 @@ public class ActionTests {
 	private String url;
 	private String domain;
 	private String adminEmail;
+	private String managerEmail;
 	private RestTemplate restTemplate;
 	
 	@LocalServerPort
@@ -39,35 +48,62 @@ public class ActionTests {
 	public void setProjectName(String projectName) {
 		this.domain = projectName;
 		this.adminEmail = "admin@" + this.domain + ".com";
+		this.managerEmail = "manager@" + this.domain + ".com";
 	}
 	
 	@PostConstruct
 	public void init() {
 		this.url = "http://localhost:" + this.port + "/acs";
 		this.restTemplate = new RestTemplate();
+		// Create Admin
+		NewUserDetails admin = new NewUserDetails();
+		admin.setAvatar(":)");
+		admin.setRole(UserRole.ADMIN);
+		admin.setEmail(this.adminEmail);
+		admin.setUsername("admin");
+		this.restTemplate.postForObject(this.url + "/users", admin, UserBoundary.class);
+		// Create Manager
+		NewUserDetails manager = new NewUserDetails();
+		manager.setAvatar(":)");
+		manager.setRole(UserRole.ADMIN);
+		manager.setEmail(this.managerEmail);
+		manager.setUsername("manager");
+		this.restTemplate.postForObject(this.url + "/users", manager, UserBoundary.class);
+	}
+	
+	@AfterClass
+	public void deleteUsers() {
+		this.restTemplate.delete(url + "/admin/users/{domain}/{email}"
+				,this.domain,this.adminEmail);
 	}
 	
 	@BeforeEach
 	public void setup() {
-		// do nothing
 	}
 	
 	@AfterEach
 	public void teardown() {
 		this.restTemplate
-			.delete(this.url + "/admin/actions/{domain}/{email}",domain,adminEmail);
+		.delete(this.url + "/admin/actions/{domain}/{email}",domain,adminEmail);
+		this.restTemplate
+			.delete(this.url + "/admin/elements/{domain}/{email}",domain,adminEmail);
 	}
 	
 	@Test
 	public void testInvokeNewActionAndValidateTheDatabaseContainsASingleAction(){
+		ElementBoundary elementBoundary = new ElementBoundary(null, "regular", true,
+				"dugma", new Date(), null, new Location(25.3, 13.2),
+				new HashMap<String,Object>());
+		elementBoundary = this.restTemplate.postForObject(this.url + "/elements/{managerDomain}/{managerEmail}"
+				, elementBoundary, ElementBoundary.class, this.domain, this.managerEmail);
 		ActionBoundary action = new ActionBoundary();
-		action.setElement(new Element(new ElementId(domain,"1234")));
+		action.setElement(new Element(new ElementId(this.domain,elementBoundary.getElementId().getId())));
 		action.setType("dugma");
-		action.setInvokedBy(new InvokedBy(new UserId(domain,adminEmail)));
+		action.setInvokedBy(new InvokedBy(new UserId(this.domain, this.managerEmail)));
 		action.setActionAttributes(new HashMap<String, Object>());
 		this.restTemplate.postForObject(this.url + "/actions",action,Object.class);
 		ActionBoundary[] actions = this.restTemplate.getForObject(this.url + "/admin/actions/{domain}/{email}",
-				ActionBoundary[].class,domain,adminEmail);
+				ActionBoundary[].class,this.domain,this.adminEmail);
 		assertThat(actions).hasSize(1);
 	}
 	
